559178fc1fd37a26cdf5f56e74175a08c72ee68d
Christian Meurin <cmmeurin@gmail.com>
2019-09-02 16:50:00 -0700

    Update spells.lua

--- spells.lua
+++ spells.lua
@@ -6,6 +6,7 @@ MuteAnnoying.mute.spells = {
 	[569494] = "sound/spells/deathknight_summonwarhorse_impact_base.ogg",
 	[569673] = "sound/spells/petcall.ogg",
 	[568271] = "sound/spells/seduction_state_head.ogg",
+	[568774] = "sound/spells/sleepimpact.ogg",
 	[568730] = "sound/spells/summonnightmare.ogg",
 	[569748] = "sound/spells/summonwarhorse.ogg",
 }
